2b1cd8e93223b6930df027b807781846133b378c
sohail <sohailkhan45981204@gmail.com>
2022-05-29 21:16:46 +0500

    extra feature added(fetch news form api)

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
   render() {
+
+    let {title , description ,imageUrl, newsUrl }=this.props;
+
     return (
-      <div>
-        <p>this is news item component</p>
+      <div className="card" style={{width: "18rem"}}>
+      <img src={imageUrl} className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">{title}...</h5>
+        <p className="card-text">{description}...</p>
+        <a href={newsUrl} target="_blank"  rel="noreferrer" className="btn-sm btn-primary">Learn More</a>
       </div>
+    </div>
     )
   }
 }
