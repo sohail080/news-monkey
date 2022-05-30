542b17b75e5cef571faa4c4b716ded431171a731
sohail <sohailkhan45981204@gmail.com>
2022-05-30 20:33:57 +0500

    more logic added(bring news data from fetch api)

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -6,10 +6,10 @@ export class Newsitem extends Component {
     let {title , description ,imageUrl, newsUrl }=this.props;
 
     return (
-      <div className="card" style={{width: "18rem"}}>
-      <img src={imageUrl} className="card-img-top" alt="..."/>
+      <div className="card">
+      <img src={!imageUrl?"https:////m.files.bbci.co.uk/modules/bbc-morph-sport-seo-meta/1.22.0/images/bbc-sport-logo.png":imageUrl} className="card-img-top" alt="..."/>
       <div className="card-body">
-        <h5 className="card-title">{title}...</h5>
+        <h5 className="card-title font-weight-bold">{title}</h5>
         <p className="card-text">{description}...</p>
         <a href={newsUrl} target="_blank"  rel="noreferrer" className="btn-sm btn-primary">Learn More</a>
       </div>
